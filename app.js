1fdb83cf290c8b6bdc300df1ab8b01d1ec62d828
Rebecca Qu <beks.qu@gmail.com>
2016-07-18 19:59:53 -0400

    Add helpers file & timestamp converter

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = {};
+var app = window.app || {};
 
 app.getUserLocation = function() {
   navigator.geolocation.getCurrentPosition(function(position) {
@@ -48,36 +48,10 @@ app.getPassTime = function(lat, lng) {
     success: function(payload) {
       console.info('GETTING PASS TIMES 🌍🌏🌎🚀');
       payload.response.map(function(passtime) {
-        console.log(convertTimestamp(passtime.risetime));
+        console.log(app.helpers.convertTimestamp(passtime.risetime));
       });
     }
   });
-}
-function convertTimestamp(timestamp) {
-  var d = new Date(timestamp * 1000),
-		yyyy = d.getFullYear(),
-		mm = ('0' + (d.getMonth() + 1)).slice(-2),
-		dd = ('0' + d.getDate()).slice(-2),
-		hh = d.getHours(),
-		h = hh,
-		min = ('0' + d.getMinutes()).slice(-2),
-		ampm = 'AM',
-		time;
-
-	if (hh > 12) {
-		h = hh - 12;
-		ampm = 'PM';
-	} else if (hh === 12) {
-		h = 12;
-		ampm = 'PM';
-	} else if (hh == 0) {
-		h = 12;
-	}
-
-	// ie: 2013-02-18, 8:35 AM
-	time = yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
-
-	return time;
 }
 
 $(function() {
